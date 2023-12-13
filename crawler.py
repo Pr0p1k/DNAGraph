95a0abd45ac3917dcc7157dbdc3766b6dcbac4b5
pr0p1k <aleksandr_prokopev@epam.com>
2023-12-13 17:39:49 +0100

    Graph drawer 1

---
+++ crawler.py
@@ -0,0 +1,167 @@
+import argparse
+import logging
+import os.path
+import re
+import time
+import urllib.request
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+import bs4.element
+
+from graph_drawer import read_kit_file
+
+NAMETABLE_CLASS = "v_phase_one-section dashboard-content"
+KIT_INFO_REGEX = "Kit:[\d\D]*<br>"
+
+
+def fetch_matches_page(code):
+    request = urllib.request.Request(
+        f"https://app.gedmatch.com/OneToManyFreeOriginalResults.php?kit={code}&xsubmit=Display+Results")
+    request.add_header("Cookie", "PHPSESSID=lr57bv29mggj4p02qnnblbuisp")
+    request.add_header("Referer", "https://app.gedmatch.com/")
+    request.add_header("Sec-Ch-Ua", '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"')
+    request.add_header("Sec-Ch-Ua-Mobile", "?0")
+    request.add_header("Sec-Ch-Ua-Platform", "macOS")
+    request.add_header("Sec-Fetch-Dest", "document")
+    request.add_header("Sec-Fetch-Mode", "navigate")
+    request.add_header("Sec-Fetch-Site", "same-origin")
+    request.add_header("Sec-Fetch-User", "?1")
+    request.add_header("User-Agent",
+                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
+    result = urllib.request.urlopen(request)
+    contents = result.read()
+    logging.info("Response headers: ", result.headers)
+    logging.getLogger().handlers[0].flush()
+
+    return contents
+
+
+def fetch_intersections_page(main_kit, rel_kit):
+    data = {
+        "kit1": main_kit,
+        "kit2": rel_kit,
+        "largest_threshold": "10",
+        "shared_threshold": "10",
+        "diff_threshold": "99",
+        "xsubmit": "Display Results"
+    }
+    request = urllib.request.Request("https://app.gedmatch.com/people_match2.php", urlencode(data).encode())
+
+    request.add_header("Cookie", "PHPSESSID=lr57bv29mggj4p02qnnblbuisp")
+    request.add_header("Referer", "https://app.gedmatch.com/")
+    request.add_header("Sec-Ch-Ua", '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"')
+    request.add_header("Sec-Ch-Ua-Mobile", "?0")
+    request.add_header("Sec-Ch-Ua-Platform", "macOS")
+    request.add_header("Sec-Fetch-Dest", "document")
+    request.add_header("Sec-Fetch-Mode", "navigate")
+    request.add_header("Sec-Fetch-Site", "same-origin")
+    request.add_header("Sec-Fetch-User", "?1")
+    request.add_header("User-Agent",
+                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
+    result = urllib.request.urlopen(request)
+    contents = result.read()
+    logging.info("Response headers: ", result.headers)
+    logging.getLogger().handlers[0].flush()
+
+    return contents
+
+
+def write_to_file(kit_info, table_lines, kit):
+    current_dir = os.path.dirname(__file__)
+
+    file = open(os.path.join(current_dir, f"tables/{kit}.csv"), "x")
+
+    # file.write(kit_info + "\n\n")
+
+    file.writelines(",".join(line) + "\n" for line in table_lines)
+
+    file.close()
+
+    logging.info(f"Written {kit} to file")
+    logging.getLogger().handlers[0].flush()
+
+
+def parse_page(page, tags_info):
+    parsed_html = BeautifulSoup(page, features="html.parser")
+    entries = filter(lambda x: isinstance(x, bs4.element.Tag),
+                     parsed_html.body.find('table', tags_info).children)
+
+    lines = []
+
+    for entry in entries:
+        cols = entry.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        lines.append(cols)
+
+    return lines
+
+
+def get_kit_info(page):
+    return re.search(KIT_INFO_REGEX, str(page)).group()[:-8]
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="GEDMatch crawler")
+    parser.add_argument("-k", "--kit")  # kit for which to get direct matches
+    parser.add_argument("-r", "--recursion_depth")  # for how many direct matches get their matches
+    parser.add_argument("-o", "--offset")  # offset to the prev param
+
+    args = parser.parse_args()
+
+    main_kit = args.kit
+    if main_kit is None:
+        print("No kit number given")
+        exit(1)
+
+    main_page = fetch_matches_page(main_kit)
+
+    lines = parse_page(main_page, None)
+
+    kit_info = get_kit_info(main_page)
+
+    write_to_file(kit_info, lines, main_kit)
+
+    codes = []
+
+    for line in lines[1:]:
+        codes.append(line[0])
+
+    # get first 5 relatives
+    for code in codes[0:5]:
+        relative_page = fetch_matches_page(code)
+        rel_lines = parse_page(relative_page)
+        write_to_file(get_kit_info(relative_page), rel_lines, code)
+        time.sleep(3)
+
+
+def intsec_main():
+    parser = argparse.ArgumentParser(prog="GEDMatch crawler")
+    parser.add_argument("-k", "--kit")  # kit for which to get direct matches
+    parser.add_argument("-r", "--recursion_depth")  # for how many direct matches get their matches
+    parser.add_argument("-o", "--offset")  # offset to the prev param
+
+    args = parser.parse_args()
+
+    main_kit = args.kit
+    if main_kit is None:
+        print("No kit number given")
+        exit(1)
+
+    reader = read_kit_file(main_kit)
+
+    kit_info = next(reader)
+    next(reader)  # skip empty line
+
+    rel_kits = []
+    for line in reader:
+        rel_kits.append(line[0])
+
+    for kit in rel_kits[50:55]:
+        intersection_page = fetch_intersections_page(main_kit, kit)
+        lines = parse_page(intersection_page, {"class": "results-table"})
+        write_to_file("kit_info", lines[2:], f"{main_kit}-{kit}")  # TODO 2 empty lines
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    intsec_main()
---
+++ graph_drawer.py
@@ -0,0 +1,106 @@
+import os
+import csv
+from typing import Dict, Any
+
+TABLES_PATH = "tables2"
+LIMIT = 53
+
+
+def read_kit_file(code):
+    current_dir = os.path.dirname(__file__)
+
+    filename = os.path.join(current_dir, f"{TABLES_PATH}/{code}.csv")
+
+    file = open(filename, "r")
+
+    csvreader = csv.reader(file)
+
+    return csvreader
+
+
+def calc_edge_color(mrca):
+    mrca = float(mrca)
+    if mrca <= 4.1:
+        return "green"
+    elif mrca <= 4.5:
+        return "yellow"
+    elif mrca <= 5.0:
+        return "orange"
+    else:
+        return "red"
+
+
+GRAPH: dict[Any, Any] = dict()
+# {
+# "code": [(code, mrca)]
+# }
+IGNORE_LIST = ["AR8211960"]
+
+
+def main():
+    me_code = "DE5484843"
+    reader = read_kit_file(me_code)
+
+    kit_info = next(reader)
+    next(reader)  # skip empty line
+
+    rel_kits = []
+    for line in reader:
+        rel_kits.append(line)
+
+    graph_file = open(os.path.dirname(__file__) + "/graph.gv", "w")
+
+    GRAPH[me_code] = []
+
+    graph_file.write("graph {\n")
+    graph_file.write("graph [outputorder=edgesfirst];\nnode [style=filled fillcolor=white];\n")
+    for kit in rel_kits[1:LIMIT]:
+        GRAPH[me_code].append((kit[0], kit[6]))
+
+    for kit in rel_kits[1:LIMIT]:
+        reader = read_kit_file(f"{me_code}-{kit[0]}")
+
+        for line in reader:
+            if len(line) == 0:
+                continue
+
+            rel_code = line[0]
+
+            if rel_code == me_code:
+                continue
+
+            mrca = line[8]
+            if kit[0] in GRAPH:
+                GRAPH[kit[0]].append((rel_code, mrca))
+            else:
+                GRAPH[kit[0]] = [(rel_code, mrca)]
+
+            if rel_code in GRAPH:
+                GRAPH[rel_code].append((kit[0], mrca))
+            else:
+                GRAPH[rel_code] = [(kit[0], mrca)]
+            # graph_file.write(f"{kit[0]} -- {rel_code}[label={mrca}]\n")  # Me -- {rel_code}[label={line[5]}]\n
+
+    # draw my direct connections
+    for kit in rel_kits[1:LIMIT]:
+        if kit[0] in GRAPH and len(GRAPH[kit[0]]) > 1:  # only those that are connected with someone else
+            graph_file.write(f"{me_code} -- {kit[0]}[label={kit[6]} color=blue]\n")
+
+    drawn_set = set()
+
+    for kit, matches in GRAPH.items():
+        if len(matches) > 1:
+            for match in matches:
+                if match[0] in GRAPH and len(GRAPH[match[0]]) > 1 \
+                        and kit != me_code and (kit, match[0]) not in drawn_set \
+                        and kit not in IGNORE_LIST and match[0] not in IGNORE_LIST:
+                    drawn_set.add((match[0], kit))
+                    graph_file.write(f"{kit} -- {match[0]}[color={calc_edge_color(match[1])}]\n")
+
+    graph_file.write("}\n")
+
+    graph_file.close()
+
+
+if __name__ == '__main__':
+    main()
